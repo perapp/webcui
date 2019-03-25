3fd32a66903c07e7df770e6508bd9083a6b9eb4c
Per Appelgren <per@appgren.se>
2019-03-25 14:31:56 +0000

    .

--- Makefile
+++ Makefile
@@ -1,6 +1,18 @@
+VENV_PY  = python3
+PYTHON   = python
+PIP      = $(PYTHON) -m pip
 
-build: install_req
-   echo "build"
+.PHONY: build venv req buildreq test
 
-install_req:
-   python -m pip install -r python/requirements.txt
+build: req
+	$(PYTHON) python/setup.py sdist bdist_wheel
+
+req:
+	$(PIP) install -r python/requirements.txt
+
+test:
+	python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+	python -m pip install --index-url https://test.pypi.org/simple/ webcui
+
+clean:
+	rm -rf build
---
+++ python/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import pathlib
+
+home = pathlib.Path(__file__).parent.parent
+readme = (home/"README.md").read_text()
+
+setuptools.setup(
+    name="webcui",
+    version="0.0.1",
+    author="Per Appelgren",
+    author_email="per@appgren.se",
+    description="A web command user interface",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/perapp/webcui",
+    packages=["webcui"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
