44a4c4c7131d49002680b975daca34dc99eedcda
Per Appelgren <per@appgren.se>
2019-03-27 04:58:35 +0000

    Update test_basic_app.py

--- test/app/test_basic_app.py
+++ test/app/test_basic_app.py
@@ -1,11 +1,13 @@
 import webcui
+import bs4 as bs
 
 def cmd(your_name):
    """A command greeting you"""
    return f"Hello {your_name}!"
 
 def test_form():
-   assert webcui.make_form(cmd) == "<form/e>"
+.  doc = bs.B.BeautifulSoup(api.requests.get("/").text)
+   assert doc.find(id="your_name") is not None
 
 if __name__ == '__main__':
    webcui.run(cmd)
