ac5e5187af39b6cae41128869e5dea7de80f39d0
Per Appelgren <per@appgren.se>
2019-03-27 12:54:54 +0000

    .

--- python/webcui/server.py
+++ python/webcui/server.py
@@ -1,15 +1,34 @@
 import responder
+from dataclasses import dataclass
+import inspect
 
-api = responder.API()
+api = responder.API(templates_dir="www")
+
+@dataclass
+class Parameter(object):
+    name: str
+    label: str
+
 
 @api.route("/")
-class Server:
-    def __init__(self, cmd):
+class ServerImpl(object):
+    def __init__(self):
         super().__init__()
-        self.cmd = cmd
-        
-    def on_request(self, req, resp):
-        resp.html = api.template("form.html")
+        self.cmd = None
+
+    def on_get(self, req, resp):
+        # TODO: self.cmd is None since constructor is called from responder.
+        print(self.cmd)
+        doc = self.cmd.__doc__
+        if not doc:
+            doc = self.cmd.__name__
+
+        spec = inspect.getfullargspec(self.cmd)
+        params = [Parameter(x, "Foo")
+                  for x in spec.args]
+        resp.html = api.template("form.html",
+                                 doc=doc,
+                                 params=params)
 
     def run(self):
         api.run(address="0.0.0.0",
@@ -18,6 +37,13 @@ class Server:
     @property
     def requests(self):
         return api.requests
+
+def Server(cmd):
+    if cmd is None:
+        raise ValueError("cmd argument must be specified")
+    server = ServerImpl()
+    server.cmd = cmd
+    return server
 
 def run(cmd):
     Server(cmd).run()
