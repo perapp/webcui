3961b47092199114e75e77bd401016c0d781cca8
Per Appelgren <per@appgren.se>
2019-03-27 14:29:38 +0000

    .

--- python/webcui/server.py
+++ python/webcui/server.py
@@ -2,23 +2,18 @@ import responder
 from dataclasses import dataclass
 import inspect
 
-api = responder.API(templates_dir="www")
-
 @dataclass
 class Parameter(object):
     name: str
     label: str
 
-
-@api.route("/")
-class ServerImpl(object):
-    def __init__(self):
-        super().__init__()
-        self.cmd = None
+class Server(object):
+    def __init__(self, cmd):
+        self.cmd = cmd
+        self.api = responder.API(templates_dir="www")
+        self.api.add_route("/", self.on_get)
 
     def on_get(self, req, resp):
-        # TODO: self.cmd is None since constructor is called from responder.
-        print(self.cmd)
         doc = self.cmd.__doc__
         if not doc:
             doc = self.cmd.__name__
@@ -26,9 +21,9 @@ class ServerImpl(object):
         spec = inspect.getfullargspec(self.cmd)
         params = [Parameter(x, "Foo")
                   for x in spec.args]
-        resp.html = api.template("form.html",
-                                 doc=doc,
-                                 params=params)
+        resp.html = self.api.template("form.html",
+                                      doc=doc,
+                                      params=params)
 
     def run(self):
         api.run(address="0.0.0.0",
@@ -36,14 +31,7 @@ class ServerImpl(object):
 
     @property
     def requests(self):
-        return api.requests
-
-def Server(cmd):
-    if cmd is None:
-        raise ValueError("cmd argument must be specified")
-    server = ServerImpl()
-    server.cmd = cmd
-    return server
+        return self.api.requests
 
 def run(cmd):
     Server(cmd).run()
--- test/app/test_basic_app.py
+++ test/app/test_basic_app.py
@@ -5,11 +5,11 @@ def cmd(your_name):
    """A command greeting you"""
    return f"Hello {your_name}!"
 
-
 server = webcui.Server(cmd)
 
 def test_form():
-   doc = bs.BeautifulSoup(server.requests.get("/").text)
+   doc = bs.BeautifulSoup(server.requests.get("/").text,
+                          features="html.parser")
    assert doc.find(id="your_name") is not None
 
 if __name__ == '__main__':
