2d94067dffe34de3b2531a32e9c417ed65968669
Per Appelgren <per@appgren.se>
2019-04-29 13:01:25 +0000

    .

--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,7 @@ import pytest
 import docker
 from docker.types import Mount
 import os
+from urllib.parse import urlparse
 
 @pytest.fixture(scope='session')
 def deploy_server():
@@ -26,8 +27,9 @@ def deploy_server():
     host_port_map = ports["22/tcp"][0]
 
     # translate HostIp to $DOCKER_HOST for when running in dind mode (e.g. in Gitlab CI)
-    if host_port_map.get("HostIp") == "0.0.0.0" and os.environ.get("DOCKER_HOST"):
-        host_port_map["HostIp"] = os.environ.get("DOCKER_HOST")
+    docker_host = urlparse(os.environ.get("DOCKER_HOST")).hostname
+    if docker_host and host_port_map.get("HostIp") == "0.0.0.0":
+        host_port_map["HostIp"] = docker_host
 
     print(f"Started {image} on {client.api.base_url} with port mapping: {container.attrs['NetworkSettings']['Ports']}")
 
