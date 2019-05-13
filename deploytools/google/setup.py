ba413355c6deb96aca80c0d42058baf6a17be5c6
Per Appelgren <per@appgren.se>
2019-05-13 18:59:46 +0000

    .

--- deploytools/google/setup.py
+++ deploytools/google/setup.py
@@ -1,7 +1,43 @@
 #!/usr/bin/python3
+import subprocess
 
-import googleapiclient
+PROJ_NAME = "webcuiproj2"
+NODE_NAME = "webcui"
 
+def run():
+    if not test_gcloud("projects", "describe", PROJ_NAME):
+        gcloud("projects", "create", PROJ_NAME)
+    gcloud("config", "set", "project", PROJ_NAME)
+    gcloud("config", "set", "compute/zone", "us-east1-c")
+
+    try:
+        gcloud("services", "enable", "compute.googleapis.com")
+    except:
+        print("Could not enable compute.googleapis.com service for your project. Usually you need to enable billing to continue.")
+        print(f"Please do so by following this link: https://console.developers.google.com/billing?project={PROJ_NAME}")
+        return
+
+    if not test_gcloud("compute", "instances", "describe", NODE_NAME):
+        gcloud("compute", "instances", "create", NODE_NAME,
+               "--machine-type=f1-micro",
+               "--tags=http-server,https-server"
+               "--metadata=ssh-keys=per:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6HL/Q198U+qO9io8AWPzggD5o9PjgZD5bV3OOXLW+vBR3PQL8OmQabjpMkbeE0CV8xACMfa8ARYzNBAtaUaPs01LqTpcJdI/EztALqS0bNq+l1Fvo7VVE83f2vp6df2CCS6Ixds3G61UIsJki7TVa1pnK389d6sFJwGSksvzGZrs5IhXwpXnRm4Iogb9YW1UJsp4Fbd7cMCz3ln3LvsIB3pBGdbTvuLwBdxu2Kvck2YhfybZcmHpNN8PnIApdcgAZ+se9Bv77EYx35Kz5aQJEnfBA4XHjc7kXLjRhK8z+rOaJPi4Gij3l6MhvaNSckmw1Cj3iG51Xvnh/eygvAtR5 per@dev-small")
+
+        
+def gcloud(*args):
+    p = subprocess.run(["gcloud"] + list(args), check=True)
+    return p.stdout
+
+
+def test_gcloud(*args):
+    return subprocess.run(["gcloud"] + list(args),
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE).returncode == 0
+
+if __name__ == "__main__":
+    run()
+
+"""
 
 def run():
     
@@ -16,3 +52,4 @@ gcloud compute instances create webcui --machine-type=f1-micro \
    --tags=http-server,https-server \
    --metadata=ssh-keys="per:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6HL/Q198U+qO9io8AWPzggD5o9PjgZD5bV3OOXLW+vBR3PQL8OmQabjpMkbeE0CV8xACMfa8ARYzNBAtaUaPs01LqTpcJdI/EztALqS0bNq+l1Fvo7VVE83f2vp6df2CCS6Ixds3G61UIsJki7TVa1pnK389d6sFJwGSksvzGZrs5IhXwpXnRm4Iogb9YW1UJsp4Fbd7cMCz3ln3LvsIB3pBGdbTvuLwBdxu2Kvck2YhfybZcmHpNN8PnIApdcgAZ+se9Bv77EYx35Kz5aQJEnfBA4XHjc7kXLjRhK8z+rOaJPi4Gij3l6MhvaNSckmw1Cj3iG51Xvnh/eygvAtR5 per@dev-small"
 
+"""
